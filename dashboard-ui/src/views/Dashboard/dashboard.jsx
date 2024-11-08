bd1235e7bedf61d230e72aab235c80390f773e76
Jordan Schultz <jmschult@stanford.edu>
2024-11-08 15:38:55 -0800

    Created Simple table for UI

--- dashboard-ui/src/views/Dashboard/dashboard.jsx
+++ dashboard-ui/src/views/Dashboard/dashboard.jsx
@@ -1,7 +1,33 @@
-import React, {useState} from "react";
-import {Button, Card, Grid, Group, Text, Stepper} from '@mantine/core';
+import React, {useState, useEffect} from "react";
+import {Button, Card, Grid, Table, Text} from '@mantine/core';
 
 export function Dashboard() {
+    const [intakes, setIntakes] = useState([])
+
+    useEffect(() => {
+        console.log('fire?')
+        fetchIntakes()
+    }, [])
+
+    const fetchIntakes = () => {
+        let jsmoModule;
+        if(import.meta?.env?.MODE !== 'development')
+            jsmoModule = ExternalModules.Stanford.IntakeDashboard
+        jsmoModule.fetchIntakeParticipation(successCallback)
+
+    }
+
+    const successCallback = (res) => {
+        console.log('success', res)
+        setIntakes(res?.data)
+    }
+
+    const tableData = {
+        caption: 'Intake List',
+        head: ['UID', 'Listed Contact Type', 'Study Title', 'PI Name', 'Completion Status', 'Detail'],
+        body: (intakes && intakes.length > 0) ? intakes.map(item => [item.intake_id, item.type, item.research_title, item.pi_name, item.intake_complete, "Detail"]) : []
+    }
+
     return (
         <div style={{width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', padding: '30px'}}>
             <Grid style={{height: '100%'}}>
@@ -10,6 +36,9 @@ export function Dashboard() {
                         <Text align="center" size="xl" weight={700}>
                             Welcome to your intake dashboard!
                         </Text>
+                        <Table
+                            data={tableData}
+                        />
                     </Card>
                 </Grid.Col>
             </Grid>
