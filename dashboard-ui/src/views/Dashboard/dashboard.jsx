b6506ec0d98494994d430a3a01a498298b25701d
Jordan Schultz <jmschult@stanford.edu>
2024-11-14 11:28:22 -0800

    Added new UI for detail page, tabular page

--- dashboard-ui/src/views/Dashboard/dashboard.jsx
+++ dashboard-ui/src/views/Dashboard/dashboard.jsx
@@ -1,9 +1,13 @@
-import React, {useState, useEffect} from "react";
-import {Button, Card, Grid, Table, Text} from '@mantine/core';
-// import {}
+import React, {useState, useEffect, useCallback} from "react";
+import {AppShell, Button, Group, Card, Image, Table, Text, Divider, Title, Blockquote, List} from '@mantine/core';
+import { IconInfoCircle } from '@tabler/icons-react';
+import {useNavigate} from "react-router-dom";
+import './dashboard.css';
+
 export function Dashboard() {
     const [intakes, setIntakes] = useState([])
     const [error, setError] = useState('')
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetchIntakes()
@@ -27,28 +31,107 @@ export function Dashboard() {
         setError(err?.error)
     }
 
-    const renderNavButton = () => <Button variant="light">Navigate</Button>
+    const transition = (id) => {
+        navigate(`/detail/${id}`)
+    }
+
+    const renderNavButton = useCallback(
+        (id) => (
+            <Button
+                id={id}
+                onClick={() => transition(id)} // Only created once per id
+                variant="light"
+            >
+                Navigate
+            </Button>
+        ),
+        [] // Dependencies are empty so this function will only be created once
+    );
+
     const tableData = {
         caption: 'List of intakes you have been added to',
         head: ['UID', 'Listed Contact Type', 'Study Title', 'PI Name', 'Completion Status', 'Detail'],
-        body: (intakes && intakes.length > 0) ? intakes.map(item => [item.intake_id, item.type, item.research_title, item.pi_name, item.intake_complete, renderNavButton()]) : []
+        body: (intakes && intakes.length > 0) ? intakes.map(item => [item.intake_id, item.type, item.research_title, item.pi_name, item.intake_complete, renderNavButton(item.intake_id)]) : []
+    }
+
+    const finishedTable = {
+        caption: 'List of completed intakes',
+        head: ['UID', 'Listed Contact Type', 'Study Title', 'PI Name', 'Completion Status', 'Detail'],
+        body: (intakes && intakes.length > 0) ? intakes.map(item => [item.intake_id, item.type, item.research_title, item.pi_name, item.intake_complete, renderNavButton(item.intake_id)]) : []
     }
 
     return (
-        <div style={{width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', padding: '30px'}}>
-            <Grid style={{height: '100%'}}>
-                <Grid.Col span={12} style={{padding: 0}}>
-                    <Card shadow="sm" p="lg" style={{height: '100%'}}>
-                        <Text align="center" size="xl" weight={700}>
-                            Welcome to your intake dashboard!
-                        </Text>
+        <AppShell
+            header={{ height: 40 }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Group h="100%" px="md">
+                    <Image src="https://storage.googleapis.com/group-chat-therapy/stanford-logo.svg"
+                           h={30}
+                           w="auto"
+                           fit="contain"
+                           alt="stanford_image"
+                    />
+
+                </Group>
+            </AppShell.Header>
+            <AppShell.Main style={{backgroundColor: 'rgb(248,249,250)'}}>
+                <Title order={3}>Welcome to your intake dashboard</Title>
+                <Text c="dimmed">Logged in as: {globalUsername}</Text>
+                <Blockquote color="blue" iconSize={36} mt="xl" radius="md" icon={<IconInfoCircle/>}>
+                    <List size="md">
+                        <List.Item>These tables represent active and completed research intakes affiliated with your username</List.Item>
+                        <List.Item>You will see an entry in either table for any submissions that list your username as a contact</List.Item>
+                        <List.Item>Missing a submission? Please contact your PI</List.Item>
+                    </List>
+                </Blockquote>
+                <Divider my="md" />
+                <Card withBorder shadow="sm" radius="md">
+                    <Card.Section withBorder inheritPadding py="xs">
+                        <Text fw={500}>Active</Text>
+                    </Card.Section>
+                    <Card.Section>
                         <Table
                             striped
                             data={tableData}
                         />
-                    </Card>
-                </Grid.Col>
-            </Grid>
-        </div>
-    );
+                    </Card.Section>
+                </Card>
+
+                <div style={{marginTop: '100px'}}></div>
+
+                <Card withBorder shadow="sm" radius="md">
+                    <Card.Section withBorder inheritPadding py="xs">
+                        <Text fw={500}>Complete</Text>
+                    </Card.Section>
+                    <Card.Section>
+                        <Table
+                            striped
+                            data={finishedTable}
+                        />
+                    </Card.Section>
+                </Card>
+
+            </AppShell.Main>
+        </AppShell>
+
+    )
+    // return (
+    //     <div style={{width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', padding: '30px'}}>
+    //         <Grid style={{height: '100%'}}>
+    //             <Grid.Col span={12} style={{padding: 0}}>
+    //                 <Card shadow="sm" p="lg" style={{height: '100%'}}>
+    //                     <Text align="center" size="xl" weight={700}>
+    //                         Welcome to your intake dashboard!
+    //                     </Text>
+    //                     <Table
+    //                         striped
+    //                         data={tableData}
+    //                     />
+    //                 </Card>
+    //             </Grid.Col>
+    //         </Grid>
+    //     </div>
+    // );
 }
--- dashboard-ui/src/views/IntakeDetail/intakeDetail.jsx
+++ dashboard-ui/src/views/IntakeDetail/intakeDetail.jsx
@@ -1,14 +1,41 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import {Button, Card, Grid, Group, Text, Stepper} from '@mantine/core';
+import {useParams} from "react-router";
+import {useNavigate} from "react-router-dom";
 
 export function IntakeDetail() {
     const stepCount = 5; // Change this number to define how many steps you want
     const [activeStep, setActiveStep] = useState(1);
-
+    const [isLoading, setIsLoading] = useState(true);
     const nextStep = () => setActiveStep((current) => (current < stepCount - 1 ? current + 1 : current));
     const prevStep = () => setActiveStep((current) => (current > 0 ? current - 1 : current));
+    const params = useParams();
+    const navigate = useNavigate()
 
+    useEffect(() => {
+        let jsmoModule;
+
+        const successCallback = (res) => {
+            console.log('success', res)
+            setIsLoading(false)
+
+        }
+
+        const errorCallback = (err) => {
+            console.error('error', err)
+            navigate('/')
+        }
+
+        if(import.meta?.env?.MODE !== 'development')
+            jsmoModule = ExternalModules.Stanford.IntakeDashboard
+
+        // globalUsername injected via root.php
+        let payload = {username: globalUsername, uid: params['id']}
+        jsmoModule.checkUserDetailAccess(payload, successCallback, errorCallback)
+    }, [])
+
+    console.log(params)
     // Generate step descriptions dynamically but with the first step being fixed
     const steps = [
         {
@@ -22,6 +49,10 @@ export function IntakeDetail() {
             content: `Step ${index + 2}: Content for this step goes here.`,
         })),
     ];
+
+    if (isLoading) {
+        return null; // Render nothing while loading
+    }
 
     return (
         <div style={{width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', padding: '30px'}}>
