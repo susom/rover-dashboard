88eac2b99b360fef2456f184d0f032937cc34259
Jordan Schultz <jmschult@stanford.edu>
2024-12-30 16:50:30 -0800

    Added activation callbacks , style

--- dashboard-ui/src/views/Dashboard/dashboard.jsx
+++ dashboard-ui/src/views/Dashboard/dashboard.jsx
@@ -5,6 +5,7 @@ import {IconExternalLink, IconInfoCircle} from '@tabler/icons-react';
 import {useNavigate} from "react-router-dom";
 import { AppHeader } from '../../components/AppHeader/appHeader'; // Import the reusable header
 import { IconPlus, IconDotsVertical, IconArrowRight, IconToggleRightFilled} from '@tabler/icons-react';
+import {TableMenu} from "../../components/TableMenu/TableMenu.jsx";
 
 import './dashboard.css';
 
@@ -13,9 +14,7 @@ export function Dashboard() {
     const [error, setError] = useState('')
     const [loading, { toggle, close }] = useDisclosure(true);
     const [newRequestLink, setNewRequestLink] = useState('')
-
     const navigate = useNavigate()
-
     useEffect(() => {
         fetchIntakes()
     }, [])
@@ -47,26 +46,41 @@ export function Dashboard() {
             let jsmoModule;
             if (import.meta?.env?.MODE !== 'development')
                 jsmoModule = ExternalModules.Stanford.IntakeDashboard;
-            jsmoModule.fetchIntakeParticipation(successCallback, errorCallback);
+            jsmoModule.fetchIntakeParticipation(intakeSuccessCallback, intakeErrorCallback);
         }
     };
 
+    const transition = (id) => {
+        navigate(`/detail/${id}`)
+    }
 
-    const successCallback = (res) => {
+    const intakeSuccessCallback = (res) => {
         console.log('success', res)
         toggle()
         setIntakes(res?.data)
         setNewRequestLink(res?.link)
     }
 
-    const errorCallback = (err) => {
+    const intakeErrorCallback = (err) => {
         console.error('error', err)
         toggle()
         setError(err?.error)
     }
 
-    const transition = (id) => {
-        navigate(`/detail/${id}`)
+    const toggleActiveCallback = (res) => {
+        setIntakes((prevIntakes) => {
+            const updatedIntakes = prevIntakes.map((item) => {
+                if (item.intake_id === res?.data?.record_id) {
+                    return {
+                        ...item,
+                        intake_active: res?.data?.intake_active,
+                        active_change_date: res?.data?.active_change_date
+                    };
+                }
+                return item;
+            });
+            return updatedIntakes;
+        });
     }
 
     const renderNavButton = useCallback(
@@ -84,43 +98,43 @@ export function Dashboard() {
         [] // Dependencies are empty so this function will only be created once
     );
 
-    const renderMenu = () => {
+    const renderMenu = (item) => {
         return (
-            <Menu position="right-start" shadow="md" width={220}>
-                <Menu.Target>
-                    <ActionIcon size="lg" variant="default" aria-label="Settings" style={{border:'none'}}>
-                        <IconDotsVertical stroke={1.5} />
-                    </ActionIcon>
-                </Menu.Target>
-                <Menu.Dropdown>
-                    <Menu.Item
-                        color="red"
-                        leftSection={<IconToggleRightFilled/>}
-                    >
-                        Deactivate
-                    </Menu.Item>
-                    <Menu.Label>Restricts functionality & editing You can reactivate at any time</Menu.Label>
-                </Menu.Dropdown>
-            </Menu>
+            <TableMenu
+                toggleSuccess={toggleActiveCallback}
+                toggleError={toggleActiveCallback}
+                rowData={item}
+            />
         )
     }
 
+    const filteredIntakes = intakes?.filter(item => item.intake_active !== "0") || [];
+    const filteredOutIntakes = intakes?.filter(item => item.intake_active === "0") || [];
     const tableData = {
         caption: 'List of intakes you have been added to',
-        head: ['', 'UID', 'Initial submission date', 'Study Title', 'PI Name', 'Status', ''],
-        body: (intakes && intakes.length > 0) ? intakes.map(item => [renderMenu(), item.intake_id, item.completion_timestamp, item.research_title, item.pi_name, item.intake_complete, renderNavButton(item.intake_id)]) : []
-    }
+        head: ['', 'UID', 'Initial Submission Date', 'Study Title', 'PI Name', 'Status', ''],
+        body: filteredIntakes.map(item => [
+            renderMenu(item),
+            item?.intake_id,
+            item?.completion_timestamp,
+            item?.research_title,
+            item?.pi_name,
+            item?.intake_complete,
+            renderNavButton(item.intake_id)
+        ])
+    };
 
-    // const finishedTable = {
-    //     caption: 'List of completed intakes',
-    //     head: ['UID', 'Initial submission date', 'Study Title', 'PI Name', 'Status', 'Detail'],
-    //     body: (intakes && intakes.length > 0) ? intakes.map(item => [item.intake_id, item.completion_timestamp, item.research_title, item.pi_name, item.intake_complete, renderNavButton(item.intake_id)]) : []
-    // }
     const finishedTable = {
-        caption: 'List of completed intakes',
-        head: ['UID', 'Initial submission date', 'Study Title', 'PI Name', 'Status', 'Detail'],
-        body: []
-    }
+        caption: 'List of filtered out intakes',
+        head: ['', 'UID', 'Activity Change Date', 'Study Title', 'PI Name'],
+        body: filteredOutIntakes.map(item => [
+            renderMenu(item),
+            item?.intake_id,
+            item?.active_change_date,
+            item?.research_title,
+            item?.pi_name,
+        ])
+    };
 
     return (
         <AppShell
@@ -164,10 +178,10 @@ export function Dashboard() {
 
                 <div style={{marginTop: '60px'}}></div>
 
-                <Card withBorder shadow="sm" radius="md">
+                <Card withBorder shadow="sm" radius="md" id="inactive-table">
                     <Card.Section withBorder inheritPadding py="xs">
                         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                            <Text fw={500}>Complete</Text>
+                            <Text fw={500}>Inactive</Text>
                             {loading && <Loader size={32} />}
                         </div>
                     </Card.Section>
