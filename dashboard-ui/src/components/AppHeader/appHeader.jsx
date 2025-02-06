301e00ff5c35ebb1ef337920268fd9f245d4d747
Jordan Schultz <jmschult@stanford.edu>
2025-02-05 21:05:19 -0800

    Added prototype tag

--- dashboard-ui/src/components/AppHeader/appHeader.jsx
+++ dashboard-ui/src/components/AppHeader/appHeader.jsx
@@ -3,14 +3,16 @@ import { Group, Image } from '@mantine/core';
 
 export function AppHeader() {
     return (
-        <Group h="100%" px="md">
-            <Image
-                src="https://storage.googleapis.com/group-chat-therapy/stanford-logo.svg"
-                h={30}
-                w="auto"
-                fit="contain"
-                alt="stanford_image"
-            />
-        </Group>
+            <Group h="100%" px="md" justify="space-between" w="100%">
+                <Image
+                    src="https://storage.googleapis.com/group-chat-therapy/stanford-logo.svg"
+                    h={30}
+                    w="auto"
+                    fit="contain"
+                    alt="stanford_image"
+                />
+                <p>Prototype</p>
+            </Group>
+
     );
 }
